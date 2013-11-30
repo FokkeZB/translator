b92d7fc484ebe46dda78b53686e481f16f28a769
Fokke Zandbergen <mail@fokkezb.nl>
2013-11-30 16:09:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Translator
+
+This is a small but helpful file in translating your ([Titanium](http://docs.appcelerator.com/titanium/latest/#!/guide/Titanium_Command-Line_Interface_Reference)) apps.
+
+# How to use
+
+1. Copy the `index.php` to a server.
+2. Secure the directory by using `.htaccess` or just giving it a random name.
+3. Copy your app's `i18n` directory to the same folder.
+4. Point people to the URL to translate the strings.
+5. Download the `i18n` directory back to your app or use [ti-i18n](https://nodei.co/npm/ti-i18n/) to merge the files with your local copies if you have made local changes as well.
+
+## License
+
+<pre>
+Copyright 2013 Fokke Zandbergen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</pre>
---
+++ index.php
@@ -0,0 +1,90 @@
+<?
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $language = $_POST['pk'];
+  $name = $_POST['name'];
+  $value = $_POST['value'];
+  $filename = './i18n/' . $language . '/strings.xml';
+
+  $xml = simplexml_load_file($filename);
+
+  if (!$xml) {
+    http_response_code(400);
+    exit('Cannot find language file: ' . $language);
+  }
+
+  $strings = $xml->xpath('/resources/string[@name="' . $name . '"]');
+
+  if (count($strings) !== 1) {
+    http_response_code(400);
+    exit('Found 0 or multiple strings named: ' + $name);
+  }
+
+  $strings[0][0] = $value;
+
+  if (!$xml->asXML($filename)) {
+    http_response_code(400);
+    exit('Could not write to: ' . $language);
+  }
+
+  exit;
+}
+
+$languages = array_slice(scandir('./i18n'), 2);
+$strings = array();
+$names = array();
+
+foreach ($languages as $language) {
+  $strings[$language] = array();
+
+  $xml = simplexml_load_file('./i18n/' . $language . '/strings.xml');
+
+  foreach ($xml->string as $key => $string) {
+    $strings[$language][(string) $string['name']] = (string) $string;
+  }
+
+  $names = array_merge($names, array_keys($strings[$language]));
+}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Translation</title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script> 
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
+    <script>
+    $(document).ready(function() {
+      $('td span').editable({
+        type: 'text',
+        mode: 'inline',
+        url: './'
+      });
+    });
+    </script>
+  </head>
+  <body>
+    <table class="table table-striped">
+      <thead>
+        <th>name</th>
+        <? foreach ($languages as $language): ?>
+          <th><?= $language ?></th>
+        <? endforeach ?>
+      </thead>
+      <tbody>
+        <? foreach ($names as $name): ?>
+          <tr>
+            <th><?= $name ?></th>
+            <? foreach ($languages as $language): ?>
+              <td><span data-pk="<?= $language ?>" data-name="<?= $name ?>"><?= isset($strings[$language][$name]) ? $strings[$language][$name] : '' ?></span></td>
+            <? endforeach ?>
+          </tr>
+        <? endforeach ?>
+      </tbody>
+    </table>
+  </body>
+</html>
